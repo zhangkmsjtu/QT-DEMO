21982c0d3f92db1e29a7a0cc67e875c75a5ee1a9
Keming Zhang <zhangkm@sjtu.edu.cn>
2023-02-11 10:03:34 +0800

    通讯软件-客户端,demo10

---
+++ QT-demo10/client.h
@@ -0,0 +1,22 @@
+﻿#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QWidget>
+#include <QTcpSocket>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class client; }
+QT_END_NAMESPACE
+
+class client : public QWidget
+{
+    Q_OBJECT
+
+public:
+    client(QWidget *parent = nullptr);
+    ~client();
+
+private:
+    Ui::client *ui;
+};
+#endif // CLIENT_H
